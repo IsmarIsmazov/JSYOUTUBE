44eac7ee7834b2a5e42cb2c9af7725730499ea2e
Ismar <ismar110501@gmail.com>
2023-09-30 23:13:00 +0600

    second lesson

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-document.write("JS sey Hello");
-console.log("JS sey Hello")
-console.info("JS sey Hello")
-console.error("JS error")
-console.warn("JS warning")
+var num1 = 5;
+var num2 = 5;
+console.log("Вычитание: " + (num1 + num2));
+console.log("Math " + Math.PI);
+console.log("Math " + Math.E);
+console.log("Math " + Math.min(13, 3, 5, 1, 0, -12, 55));
+console.log("Math " + Math.max(13, 3, 5, 1, 0, -12, 55));
