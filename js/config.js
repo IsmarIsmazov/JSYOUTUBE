af5b2292294f3d056268a81a5cf430fceb922e3b
Ismar <ismar110501@gmail.com>
2023-09-30 23:40:09 +0600

    for, while, do while

--- js/config.js
+++ js/config.js
@@ -1,12 +1,16 @@
-var string = "password"
-switch (string) {
-    case "Word":
-        console.log("ok");
-        break;
-    case "password":
-        console.log("login");
-        break;
-    default:
-        console.log("no ok")
-        break;
+// for (var i = 100; i >= 2; i/=2) {
+//     console.log(i)
+// }
+// var num = 0
+// while (num < 10){
+//     num++
+//     console.log(num)
+// }
+
+var x = 0
+do {
+    console.log(x)
+    x++
+
+} while (x <=10) {
 }
